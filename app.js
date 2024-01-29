77b2452ea39cba9db71d3469465228504e9d7bfe
Shehzad-Ur-Rehman <shahzadurrehman07@gmail.com>
2024-01-29 14:24:39 +0500

    First Assignment

---
+++ app.js
@@ -0,0 +1,186 @@
+// JAVASCRIPT ASSIGNMENT-1
+
+
+
+
+// Q1.
+
+// Answer
+alert("Welcome to Our Website");
+
+
+
+// Q2. 
+
+
+
+// Q3.
+
+
+
+// Q4. 
+
+// Answer
+alert("Hello");
+alert("Shehzad");
+alert("Welcome");
+
+
+// Q5.
+
+
+
+// Q6.
+
+
+
+// Q7. 
+
+// Answer
+var username = ("Shehzad");
+console.log(username);
+
+
+
+
+// Q8. 
+
+// Answer
+var myName = ("Shehzad Ur Rehman")
+console.log(myName);
+
+
+
+
+// Q9. 
+
+// Answer
+var messege;
+
+messege = "Hello World"
+
+alert(messege);
+
+
+
+// Q10.
+
+
+
+// Q11.
+
+
+
+// Q12.
+
+
+
+// Q13.
+
+
+
+
+// Q14. 
+
+// Answer
+var age = 19;
+
+alert("Your Age is" + age);
+
+
+
+
+// Q15.
+
+
+
+
+
+// Q16. 
+
+// Answer
+var birthyear = 1999;
+
+document.write("My Birth Year is " + birthyear);
+
+
+
+// Q17.
+
+
+
+
+// Q18. 
+
+// Answer
+var shah = "Shehzad";
+var qas = "Qasim";
+var jun = "Junaid";
+
+document.write("Mr. " + shah, " Mr. " + qas, " Mr. " + jun);
+
+
+
+// Q19. 
+
+// Answer
+// legal Variable Names:
+
+// var name
+// var _date
+// var totalPrice
+// var $amount
+// var admin321
+
+
+// Illegal Variable Names:
+
+// var 123admin            (Variable names cannot start with a number)
+// var my-name             (Hyphens are not allowed in variable names)
+// var special@character   (@ is not allowed in variable names)
+//  var my account         (Spaces are not allowed in variable names)
+// var if                  (Using reserved keywords like if as variable names is not allowed)
+
+
+
+
+// Q20.
+
+
+
+
+
+// Q21. 
+
+// Answer
+var num1 = 3
+num1 = 5
+
+var num2 = 6
+num2 = 8
+
+document.write(num1 + 7, num2 + 8);
+
+
+
+// Q22.
+
+
+
+// Q23.
+
+
+
+
+// Q24. 
+
+// Answer
+var ticketprice = 600
+var totalcost = ticketprice;
+
+console.log(totalcost * 5);
+
+
+
+
+// Q25.
