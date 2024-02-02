f9ce309595cf85537b1fbe70d7648257049e0cd5
Shehzad-Ur-Rehman <shahzadurrehman07@gmail.com>
2024-02-02 17:28:27 +0500

    Assignment-2

---
+++ Assignment-2/app.js
@@ -0,0 +1,30 @@
+// Q1.
+// Answer
+
+
+
+// Q2.
+// Answer
+
+
+// Q3.
+// Answer
+// var name = prompt("What is Your Name?")
+// console.log(name + " Welcome to our Webpage");
+
+
+// Q4.
+// Answer
+// var table = prompt("enter your number"); {
+//     if (isNaN(table)) {
+//         table = 5;
+//     }
+//     for (var i = 1; i <= 10; i++){
+//         console.log(table + "x" + i + "=" + (num * i));
+//     }
+// };
+
+
+// Q5. 
+// Answer
+
