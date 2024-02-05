9a635c9627da80cfc5b07b5b26cfeed6b35a8c50
Shehzad-Ur-Rehman <shahzadurrehman07@gmail.com>
2024-02-05 08:25:25 +0500

    JavaScript-Assignment-2

--- Assignment-2/app.js
+++ Assignment-2/app.js
@@ -35,7 +35,7 @@
 //         table = 5;
 //     }
 //     for (var i = 1; i <= 10; i++){
-//         console.log(table + "x" + i + "=" + (num * i));
+//         console.log(table + "x" + i + "=" + (table * i));
 //     }
 // };
 
